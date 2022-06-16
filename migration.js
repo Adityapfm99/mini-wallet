2448ce8aeffbd9c0860147184b88718d7bec34c7
aditya <adityapfm99@gmail.com>
2022-06-17 06:52:05 +0700

    migration db

---
+++ migration.js
@@ -0,0 +1,11 @@
+const document ={
+    id : "20f2a505-f536-4d1e-a7d3-b52c4708241a",
+    ownedBy : "b7f0645c-7f8b-4588-af1e-1bec4633cc2b",
+    customerXid : "ea0212d3-abd6-406f-8c67-868e814a2436",
+    status : "enable",
+    enableAt : "2022-06-17 06:11:26",
+    disableAt : "2022-06-17 06:12:34",
+    balance : 0
+};
+
+db.wallet.insert(document);
